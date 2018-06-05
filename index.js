539f0e500a7322e28b3a15c56ceca96993406432
kyle wood <kylewood777@oru.edu>
2018-06-05 13:45:25 -0400

    Homework day 1 improved

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-console.log("testing");
-
 function changeHeader(){
-  const heading = document.querySelector('#secondHeader')
-  heading.textContent = document.getElementById('input').value
+  document.querySelector('#secondHeader').textContent = document.querySelector('#input').value
 }
-
-const input = document.getElementById("input");
