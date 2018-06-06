05ed315236c8d6451164026b166c60a046230efb
kyle wood <kylewood777@oru.edu>
2018-06-06 10:12:27 -0400

    add the ability to add the spell color to the list on a new line

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const input=document.querySelector('#spell');
 const color=document.querySelector('#effectColor');
 const spells=document.querySelector('#spells');
 
-function changeHeader(){
-    var spellItem=document.createElement("li");
-    var spellSpan=document.createElement('span');
-    var spellNameContent=document.createTextNode(input.value);
+function createSpell(){
+    const spellItem=document.createElement("li");
+    const spellSpan=document.createElement('span');
+    spellSpan.className='spellName';
+    const spellNameContent=document.createTextNode(input.value);
     spellSpan.appendChild(spellNameContent);
     spellItem.appendChild(spellSpan);
+    spellItem.appendChild(document.createElement("br"));
+    spellItem.appendChild(createSpellColor());
     spells.appendChild(spellItem);
     input.value='';
     color.value='';
+}
+
+function createSpellColor(){
+    const spellSpan=document.createElement('span');
+    const spellColorContent=document.createTextNode(color.value);
+    spellSpan.appendChild(spellColorContent);
+    spellSpan.className='spellColor';
+    return spellSpan;
 }
