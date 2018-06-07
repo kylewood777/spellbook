2cabac95cb680baccaead5eedba8d316cca5caec
kyle wood <kylewood777@oru.edu>
2018-06-06 23:32:16 -0400

    change everything to the way it was done in class

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
-const spellNameField=document.querySelector('#spellNameField');
-const spellColorField=document.querySelector('#spellColorField');
-const spellList=document.querySelector('#spellList');
-
-function createSpell(){
-    if(spellNameField.value){
-        const spellItem=document.createElement("li");
-        const spellNameSpan=document.createElement('span');
-        spellNameSpan.className='spellName';
-        spellNameSpan.appendChild(document.createTextNode(spellNameField.value));
-        spellItem.appendChild(spellNameSpan);
-        spellItem.appendChild(document.createElement("br"));
-        spellItem.appendChild(createSpellColor());
-        spellList.appendChild(spellItem);
-        spellNameField.value='';
-        spellColorField.value='';
-    }
-}
-
-function createSpellColor(){
-    const spellColorSpan=document.createElement('span');
-    spellColorSpan.appendChild(document.createTextNode(spellColorField.value));
-    spellColorSpan.className='spellColor';
-    return spellColorSpan;
-}
+const app = {
+    init: function() {
+      const form = document.querySelector('form')
+      form.addEventListener('submit', (ev) => {
+        ev.preventDefault()
+        this.handleSubmit(ev)
+      })
+    },
+  
+    renderProperty: function(name, value) {
+      const el = document.createElement('span')
+      el.classList.add(name)
+      el.textContent = value
+      el.setAttribute('title', value)
+      return el
+    },
+  
+    renderItem: function(spell) {
+      // ['name', 'level']
+      const properties = Object.keys(spell)
+  
+      // collect an array of <span> elements
+      const childElements = properties.map((prop) => {
+        return this.renderProperty(prop, spell[prop])
+      })
+  
+      const item = document.createElement('li')
+      item.classList.add('spell')
+  
+      // append each <span> to the <li>
+      childElements.forEach(function(el) {
+        item.appendChild(el)
+      })
+  
+      return item
+    },
+  
+    handleSubmit: function(ev) {
+      const f = ev.target
+  
+      const spell = {
+        name: f.spellName.value,
+        level: f.level.value,
+      }
+  
+      const item = this.renderItem(spell)
+  
+      const list = document.querySelector('#spells')
+      list.appendChild(item)
+  
+      f.reset()
+    },
+  }
+  
+  app.init()
